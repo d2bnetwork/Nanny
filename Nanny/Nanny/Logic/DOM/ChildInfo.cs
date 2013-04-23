50902d2b93cf9be9880960499d73004bacd7bb0c
martin <unknown-user@192.168.0.171>
2013-04-23 17:04:08 -0200

    Propiedades ChildInfo

--- Nanny/Nanny/Logic/DOM/ChildInfo.cs
+++ Nanny/Nanny/Logic/DOM/ChildInfo.cs
@@ -1,13 +1,27 @@
 using System;
 
-namespace Nanny
+namespace Nanny.DOM
 {
 	public class ChildInfo
 	{
-		string peteco = "";
+		enum E_Gender
+		{
+			Boy =0,
+			Girl =1
+		}
+		public int ID {get;set;}
+		public string Name {get;set;}
+		public string LastName {get;set;}
+		public string Avatar {get;set;}
+		public E_Gender Gender {get;set;}
+		public ParentInfo Parent {get;set;}
+		public List<ParentInfo> Parents{get;set;}
+
 		public ChildInfo ()
 		{
+
 		}
+
 	}
 }
 
